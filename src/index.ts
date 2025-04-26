a658981da1f182a8a0a6a544675b19f95a3a2e53
Ali Raza Khalid <razamindset.official@gmail.com>
2025-04-26 21:38:48 +0500

    Add: Simple cors and key protection

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,49 @@
 import express from "express";
-// import dotenv from "dotenv";
-// dotenv.config();
-
 import apiRouter from "./api";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+const SECRET_KEY = process.env.SECRET_KEY || null;
+const ALLOWED_CLIENT_ORIGIN =
+    process.env.ALLOWED_CLIENT_ORIGIN || "https://knightly-flax.vercel.app";
+
 app.use(express.json());
 
-app.use("/static", express.static("dist/public"), express.static("src/public"));
+const corsOptions = {
+    origin: ALLOWED_CLIENT_ORIGIN,
+    methods: ["GET", "POST"],
+};
+
+app.use(cors(corsOptions));
+
+// Secret key middleware for API routes
+app.use("/api", (req, res, next) => {
+    const secret = req.headers["x-api-secret"];
+
+    if (secret !== SECRET_KEY) {
+        return res.status(403).json({ error: "Forbidden: Invalid secret key" });
+    }
+
+    next(); // Secret is correct → move to actual API router
+});
 
 app.use("/api", apiRouter);
 
 app.get("/", (_req, res) => {
-    return res.send("Express Typescript on Vercel");
+    return res.send("Welcome to chess game review server. Author:Wintrcat");
 });
 
 app.get("/ping", (_req, res) => {
+    if (!SECRET_KEY) {
+        return res.send("Server is having some issues");
+    }
     return res.send("pong 🏓");
 });
-
-const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log("Server running on http://localhost:3000");
